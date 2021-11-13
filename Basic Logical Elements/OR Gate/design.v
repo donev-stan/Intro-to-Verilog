a87965ecefd5a725cd9c8a484643508aae88dd85
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2021-11-13 22:36:35 +0200

    OR Gate

---
+++ Basic Logical Elements/OR Gate/design.v
@@ -0,0 +1,8 @@
+module my_or_gate (
+  input A, B,
+  output C
+);
+  
+  assign C = A | B;
+  
+endmodule
---
+++ Basic Logical Elements/OR Gate/testbench.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+
+module test_bench ();
+  
+  reg  tb_A, tb_B;
+  wire tb_C;
+
+  my_or_gate u_my_or_gate (
+    .A(tb_A),
+    .B(tb_B),
+    .C(tb_C)
+  );
+  
+  initial begin
+    tb_A = 0;
+    tb_B = 0;
+    #10
+    tb_A = 1;
+    #20
+    tb_B = 1;
+    #10
+    tb_B = 0;
+    #30
+    tb_A = 0;
+    #10
+    $finish();
+  end
+  
+  initial begin
+ 	$dumpfile("dump.vcd");
+    $dumpvars;
+  end
+    
+endmodule
